c57711e56e9edd9303f251dd9c27ac6c66f5c7b1
SirG <sirgittarus@gmail.com>
2022-09-14 07:22:52 +0100

    fixed warehouse seeder

--- database/seeders/WarehouseSeeder.php
+++ database/seeders/WarehouseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Warehouse;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class WarehouseSeeder extends Seeder
 {
@@ -14,17 +16,24 @@ class WarehouseSeeder extends Seeder
      */
     public function run()
     {
-        Warehouse::create([
+        $items = [[
             'name' => 'Main Warehouse',
-            'address' => 'Okwei Street'
+            'address' => 'Okwei Street',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ],[
             'name' => 'Woman Sense Warehouse',
-            'address' => 'Frontline Main Market'
+            'address' => 'Frontline Main Market',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ],[
             'name' => 'Jasmine Plaza',
-            'address' => 'Opposite Mosque Onitsha Main Market'
-            ]
-            );
+            'address' => 'Opposite Mosque Onitsha Main Market',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]];
+
+        DB::table('warehouses')->insert($items);
 
     }
 }
